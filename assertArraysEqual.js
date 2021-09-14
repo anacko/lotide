762a5f3654e5107b39058bf4b7e059cf7e0b3a22
Ana Carolina K <anacko@gmail.com>
2021-09-14 20:58:24 +0000

    Added function: Assertion for arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+
+const assertArraysEqual = function(a, b) {
+  let assertion = true;
+  if (a.length !== b.length) {
+    assertion = false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      assertion = false;
+    }
+  }
+  if (assertion) {
+    console.log(`✅ Assertion Passed: ${a} === ${b}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${a} !== ${b}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // pass
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // pass
+assertArraysEqual([], []); // pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]); // fail
