257fb1194eefae0fa641f0cab51ce1ba6f4b0822
Ana Carolina K <anacko@gmail.com>
2021-09-16 17:20:09 -0400

    adjustments (eslint)

--- map.js
+++ map.js
@@ -22,21 +22,21 @@ const map = function(array, callback) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
 const words = ["ground", "control", "to", "major", "tom"];
-console.log("Just words: ",words)
-assertArraysEqual(map(words, word => word[0]), ["g", "c", "t", "m", "t"]); // pass
+console.log("Just words: ",words);
+assertArraysEqual(map(words, word => word[0]), ["g", "c", "t", "m", "t"]);
 
 const mix = ["123", "", "AAA"];
-console.log("A mix: ", mix)
-assertArraysEqual(map(mix, item => item[0]), ["1", , "A"]); // pass
+console.log("A mix: ", mix);
+assertArraysEqual(map(mix, item => item[0]), ["1", , "A"]);
 
-const cheer = ["Yes!", "Marvelous!", "Champion!", "Astonishing!"]
+const cheer = ["Yes!", "Marvelous!", "Champion!", "Astonishing!"];
 console.log("Just Cheering: ", cheer);
-assertArraysEqual(map(cheer, item => item[0]), ["Y", "M", "C", "A"])
+assertArraysEqual(map(cheer, item => item[0]), ["Y", "M", "C", "A"]);
 
 const story = ["Once", "upon", "a", "time..."];
-console.log("The story begins: ", story)
-assertArraysEqual(map(story, item => item[item.length-1]), ["e", "n", "a", "\."]); // pass
+console.log("The story begins: ", story);
+assertArraysEqual(map(story, item => item[item.length - 1]), ["e", "n", "a", "."]);
 
