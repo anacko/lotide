21d9983a3883f26c7a985af508e13d22903c3e6f
Ana Carolina K <anacko@gmail.com>
2021-09-20 14:35:15 -0400

    refactored to test with chai instead of with my own comparisons

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,38 @@
-const assertEqual = require("../assertEqual");
-const head = require("../head")
+const { assert } = require('chai');
+const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7]), 7);
-assertEqual(head("Hello"), "H");
-assertEqual(head([]), undefined);
-assertEqual(head("1"), "1");
+describe("#head", () => {
+  
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  
+  it("returns 7 for [7]", () => {
+    assert.strictEqual(head([7]), 7);
+  });
+  
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6 ,7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns 'H' for 'Hello'", () => {
+    assert.strictEqual(head('Hello'), 'H');
+  });
+
+  it("returns 'W' for 'Works with Strings!'", () => {
+    assert.strictEqual(head('Works with Strings!'), 'W');
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
