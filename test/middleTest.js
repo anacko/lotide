e57b1d6421fc49edd9a73910e930a1763b1498ea
Ana Carolina K <anacko@gmail.com>
2021-09-20 15:55:02 -0400

    refactored to meet chai-style-testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,46 @@
-const assertEqual = require("../assertEqual");
-const assertArraysEqual = require("../assertArraysEqual");
-
+const { assert, expect } = require('chai');
 const middle = require("../middle");
 
-assertEqual(middle([1, 2, 3]), 2);
-assertEqual(middle([8, 3, "1", 6, 2]), "1");
-assertArraysEqual(middle([2, 5, 3, 1]), [5, 3]);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe('middle', () => {
+
+  it("should return a single element if length is odd", () => {
+    const param = [1, 2, 3];
+    expect(middle(param)).to.not.be.extensible; // primitives are never extensible
+  });
+
+  it("should return an array if length is even", () => {
+    const param = [1, 2, 3, 4];
+    expect(middle(param)).to.be.an('array');
+  });
+
+  it("should return 2 for [1, 2, 3]", () => {
+    const param = [1, 2, 3];
+    const result = 2;
+    expect(middle(param)).to.equal(result);
+  });
+
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    const param = [1, 2, 3, 4];
+    const result = [2, 3];
+    expect(middle(param)).to.deep.equal(result);
+  });
+
+  it("should return [] for [1]", () => {
+    const param = [1];
+    const result = [];
+    expect(middle(param)).to.deep.equal(result);
+  });
+
+  it("should return [] for [1, 2]", () => {
+    const param = [1, 2];
+    const result = [];
+    expect(middle(param)).to.deep.equal(result);
+  });
+
+  it("should return [] for []", () => {
+    const param = [];
+    const result = [];
+    expect(middle(param)).to.deep.equal(result);
+  });
+
+ });
--- test/tailTest.js
+++ test/tailTest.js
@@ -3,25 +3,25 @@ const tail = require('../tail');
 
 describe('#tail', () => {
 
-  it('should not alter original array', () => { 
+  it('should not alter original array', () => {
     const param = [1, 2, 3];
     tail(param);
     expect(param).to.deep.equal(param);
   });
 
-  it('should return [2, 3] for [1, 2, 3]', () => { 
+  it('should return [2, 3] for [1, 2, 3]', () => {
     const param = [1, 2, 3];
     const result = [2, 3];
     expect(tail(param)).to.deep.equal(result);
   });
 
-  it('should return ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => { 
+  it('should return ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
     const param = ["Hello", "Lighthouse", "Labs"];
     const result = ["Lighthouse", "Labs"];
     expect(tail(param)).to.deep.equal(result);
   });
 
-  it('should return [] for []', () => { 
+  it('should return [] for []', () => {
     const param = [];
     const result = [];
     expect(tail(param)).to.deep.equal(result);
